2048c7971c20a7d96e6c28423dd6f433f2614c0d
miaecle <zqwu@stanford.edu>
2018-09-04 16:36:32 -0700

    minor cuts

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import os
 import h5py
 import numpy as np
 
-
+os.environ['KERAS_BACKEND'] = 'tensorflow'
 NUM_EPOCHS = 1
 BATCH_SIZE = 600
 LATENT_DIM = 292
@@ -26,40 +26,35 @@ def get_arguments():
                         help='Seed to use to start randomizer for shuffling.')
     return parser.parse_args()
 
-def main():
-    args = get_arguments()
-    np.random.seed(args.random_seed)
+np.random.seed(123)#args.random_seed)
 
-    from molecules.model import MoleculeVAE
-    from molecules.utils import one_hot_array, one_hot_index, from_one_hot_array, \
-        decode_smiles_from_indexes, load_dataset
-    from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
-    
-    data_train, data_test, charset = load_dataset(args.data)
-    model = MoleculeVAE()
-    if os.path.isfile(args.model):
-        model.load(charset, args.model, latent_rep_size = args.latent_dim)
-    else:
-        model.create(charset, latent_rep_size = args.latent_dim)
+from molecules.model import MoleculeVAE
+from molecules.utils import one_hot_array, one_hot_index, from_one_hot_array, \
+    decode_smiles_from_indexes, load_dataset
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 
-    checkpointer = ModelCheckpoint(filepath = args.model,
-                                   verbose = 1,
-                                   save_best_only = True)
+data_train, data_test, charset = load_dataset('./data/smiles_50k.h5')#args.data)
+model = MoleculeVAE()
+if os.path.isfile(args.model):
+    model.load(charset, args.model, latent_rep_size = args.latent_dim)
+else:
+    model.create(charset, latent_rep_size = args.latent_dim)
 
-    reduce_lr = ReduceLROnPlateau(monitor = 'val_loss',
-                                  factor = 0.2,
-                                  patience = 3,
-                                  min_lr = 0.0001)
+checkpointer = ModelCheckpoint(filepath = args.model,
+                               verbose = 1,
+                               save_best_only = True)
 
-    model.autoencoder.fit(
-        data_train,
-        data_train,
-        shuffle = True,
-        nb_epoch = args.epochs,
-        batch_size = args.batch_size,
-        callbacks = [checkpointer, reduce_lr],
-        validation_data = (data_test, data_test)
-    )
+reduce_lr = ReduceLROnPlateau(monitor = 'val_loss',
+                              factor = 0.2,
+                              patience = 3,
+                              min_lr = 0.0001)
 
-if __name__ == '__main__':
-    main()
+model.autoencoder.fit(
+    data_train,
+    data_train,
+    shuffle = True,
+    nb_epoch = args.epochs,
+    batch_size = args.batch_size,
+    callbacks = [checkpointer, reduce_lr],
+    validation_data = (data_test, data_test)
+)
